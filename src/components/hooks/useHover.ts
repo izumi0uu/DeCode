55c93cc3734053707a45a80f2b2a45d77cd91177
gao_yangkai <zard200107@gmail.com>
2025-02-17 19:05:28 +0800

    feat: popoverBtn

--- src/components/hooks/useHover.ts
+++ src/components/hooks/useHover.ts
@@ -4,21 +4,39 @@ import { useState, useEffect, useRef } from "react";
 
 /**
  * 监听元素是否被悬停
- * @returns [ref, isHovered]
+ * @returns isHovered
  */
 export const useHover = <T extends HTMLElement>(
   ...refs: React.RefObject<T>[]
 ) => {
   const [isHovered, setIsHovered] = useState(false);
-  const ref = useRef<T>(null);
+  const currentElements = useRef<Set<HTMLElement>>(new Set());
 
   useEffect(() => {
     const elements = refs.map((ref) => ref.current).filter(Boolean);
     if (elements.length === 0) return;
 
-    const handleEnter = () => setIsHovered(true);
+    const handleEnter = (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+      currentElements.current.add(target);
+      console.log("currentElements", currentElements.current);
+      setIsHovered(true);
+    };
+
     const handleLeave = (e: MouseEvent) => {
-      if (!elements.some((el) => el?.contains(e.relatedTarget as Node))) {
+      const target = e.target as HTMLElement;
+      const relatedTarget = e.relatedTarget as Node;
+
+      // 从当前悬停元素集合中移除离开的元素
+      currentElements.current.delete(target);
+
+      // 如果relatedTarget为null或不在监听的元素中，且没有其他元素被悬停
+      if (
+        (!relatedTarget ||
+          !elements.some((el) => el?.contains(relatedTarget))) &&
+        currentElements.current.size === 0
+      ) {
+        console.log("leave");
         setIsHovered(false);
       }
     };
