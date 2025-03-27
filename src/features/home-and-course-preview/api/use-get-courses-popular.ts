663210c9bc1dd0a85503b333dee682b99dcf8f6a
gao_yangkai <zard200107@gmail.com>
2025-03-27 22:20:12 +0800

    feat: api

--- src/features/home-and-course-preview/api/use-get-courses-popular.ts
+++ src/features/home-and-course-preview/api/use-get-courses-popular.ts
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import type { CourseDetail } from "@/features/types/course";
+import type { CourseResponse } from "@/features/types/api/course";
 
 export const fetchPopularCourses = async () => {
   const response = await fetch(`/api/courses/popular`);
   if (!response.ok) throw new Error("Failed to fetch popular courses");
-  return response.json() as Promise<CourseDetail[]>;
+  return response.json() as Promise<CourseResponse[]>;
 };
 
 export const usePopularCourses = () => {
