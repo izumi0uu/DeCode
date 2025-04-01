41674ff9bc771f267ed363abe3240aedfa7834c5
gao_yangkai <zard200107@gmail.com>
2025-04-01 18:10:33 +0800

    feat: tooltips

--- src/features/home-and-course-preview/components/animate-courses-boxes/index.tsx
+++ src/features/home-and-course-preview/components/animate-courses-boxes/index.tsx
@@ -1,6 +1,9 @@
 import { Flex } from "@/once-ui/components";
+import { useCourseCarousel } from "../../context/courseCarouselContext";
+import styles from "./index.module.scss";
 
 const AnimateCoursesBoxes = () => {
+  const { courses } = useCourseCarousel();
   return <Flex></Flex>;
 };
 
