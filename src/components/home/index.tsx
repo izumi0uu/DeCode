3deeb49daca433977780972c8f484d95d12120a9
gao_yangkai <zard200107@gmail.com>
2025-04-05 21:50:12 +0800

    home-component

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -16,55 +16,57 @@ const Home = () => {
   const cover1 = "/images/cover1.jpg";
 
   return (
-    <Column maxWidth="m" gap="xl" horizontal="center">
+    <Column>
       <Column fillWidth paddingY="l" gap="m">
-        <Column maxWidth="s">
-          <RevealFx
-            translateY="4"
-            fillWidth
-            horizontal="end"
-            paddingBottom="m"
-            gap="m"
-          >
-            <SmartImage src={cover1} alt="cover" />
-            <Heading wrap="balance" variant="display-strong-l">
-              {website.headline}
-            </Heading>
-          </RevealFx>
-          <RevealFx
-            translateY="8"
-            delay={0.2}
-            fillWidth
-            horizontal="start"
-            paddingBottom="m"
-          >
-            <Text
-              wrap="balance"
-              onBackground="neutral-weak"
-              variant="heading-default-xl"
+        <Column maxWidth="l" gap="xl" horizontal="center">
+          <Column maxWidth="s">
+            <RevealFx
+              translateY="4"
+              fillWidth
+              horizontal="end"
+              paddingBottom="m"
+              gap="m"
             >
-              {website.subline}
-            </Text>
-          </RevealFx>
-          <RevealFx translateY="12" delay={0.4} horizontal="start">
-            <Button
-              id="about"
-              data-border="rounded"
-              href="/courses"
-              variant="secondary"
-              size="m"
-              arrowIcon
+              <SmartImage src={cover1} alt="cover" />
+              <Heading wrap="balance" variant="display-strong-l">
+                {website.headline}
+              </Heading>
+            </RevealFx>
+            <RevealFx
+              translateY="8"
+              delay={0.2}
+              fillWidth
+              horizontal="start"
+              paddingBottom="m"
             >
-              <Flex gap="8" vertical="center">
-                <Avatar
-                  style={{ marginLeft: "-0.75rem", marginRight: "0.25rem" }}
-                  src={courses.picUrl}
-                  size="m"
-                />
-                {courses.button}
-              </Flex>
-            </Button>
-          </RevealFx>
+              <Text
+                wrap="balance"
+                onBackground="neutral-weak"
+                variant="heading-default-xl"
+              >
+                {website.subline}
+              </Text>
+            </RevealFx>
+            <RevealFx translateY="12" delay={0.4} horizontal="start">
+              <Button
+                id="about"
+                data-border="rounded"
+                href="/courses"
+                variant="secondary"
+                size="m"
+                arrowIcon
+              >
+                <Flex gap="8" vertical="center">
+                  <Avatar
+                    style={{ marginLeft: "-0.75rem", marginRight: "0.25rem" }}
+                    src={courses.picUrl}
+                    size="m"
+                  />
+                  {courses.button}
+                </Flex>
+              </Button>
+            </RevealFx>
+          </Column>
         </Column>
 
         {/* Featured Courses and Lessons Section */}
