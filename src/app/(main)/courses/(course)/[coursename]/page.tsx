c07412adac33ba0eb899dee54fa09a9469484f9b
gao_yangkai <zard200107@gmail.com>
2025-04-09 18:59:49 +0800

    feat: refatoring

--- src/app/(main)/courses/(course)/[coursename]/page.tsx
+++ src/app/(main)/courses/(course)/[coursename]/page.tsx
@@ -10,29 +10,29 @@ export default async function CoursePage({
 }: {
   params: { coursename: string };
 }) {
-  const coursename = params.coursename;
+  try {
+    const coursename = params.coursename;
 
-  // 找到当前课程
-  const currentCourse = coursesData.courses.find(
-    (c: Course) => c.slug === coursename || c.id.toString() === coursename
-  );
+    // 找到当前课程
+    const currentCourse = coursesData.courses.find(
+      (c: Course) => c.slug === coursename || c.id.toString() === coursename
+    );
 
-  if (!currentCourse) {
+    // 找到当前课程的所有课时
+    const courseLessons = coursesData.lessons.filter(
+      (l) => l.course?.id === currentCourse.id
+    );
+
+    return (
+      <CoursePageClient
+        course={currentCourse}
+        lessons={courseLessons}
+        coursename={coursename}
+      />
+    );
+  } catch (error) {
     throw new Error(
       "There was a problem loading this course. Please try again later."
     );
   }
-
-  // 找到当前课程的所有课时
-  const courseLessons = coursesData.lessons.filter(
-    (l) => l.course?.id === currentCourse.id
-  );
-
-  return (
-    <CoursePageClient
-      course={currentCourse}
-      lessons={courseLessons}
-      coursename={coursename}
-    />
-  );
 }
