109b6efef38f29503915cdbe3ac82cdfa2b3c96b
gao_yangkai <zard200107@gmail.com>
2025-04-09 16:55:26 +0800

    feat: server api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,5 @@ export default async function Home() {
   // 服务端数据获取
   const coursesData = await getCoursesAndLessonsForPreview();
 
-  return (
-    <Suspense fallback={<div style={{ height: "100vh" }}></div>}>
-      <HomePageClient coursesData={coursesData} />
-    </Suspense>
-  );
+  return <HomePageClient coursesData={coursesData} />;
 }
